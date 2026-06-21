c23b6e7fd5c9607a4f4e9590adc64c55d3284450
Bronas <phil42092009@gmail.com>
2026-06-21 14:20:57 +0200

    Android Orientation Change Support

--- VpMobilPlusPlus.Android/MainActivity.cs
+++ VpMobilPlusPlus.Android/MainActivity.cs
@@ -1,7 +1,10 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Content.PM;
+using Android.Content.Res;
 using Avalonia;
 using Avalonia.Android;
+using VpMobilPlusPlus.Util;
 
 namespace VpMobilPlusPlus.Android;
 
@@ -13,4 +16,15 @@ namespace VpMobilPlusPlus.Android;
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity
 {
+    public override void OnConfigurationChanged(Configuration newConfig)
+    {
+        base.OnConfigurationChanged(newConfig);
+
+        var metrics = Resources!.DisplayMetrics;
+
+        var width = metrics.WidthPixels;
+        var height = metrics.HeightPixels;
+
+        DeviceResolution.Raise(width, height);
+    }
 }
